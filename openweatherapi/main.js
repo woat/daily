3f7086f59363dc3c14bbbaed8474385c29ae2253
woat <ahmad@navlys.com>
2017-05-12 03:49:02 -0400

    openweatherapi

--- openweatherapi/main.js
+++ openweatherapi/main.js
@@ -1,16 +1,21 @@
 const button = document.getElementById('button'),
+      city = document.getElementById('city'),
+      icon = document.getElementById('icon'),
+      main = document.getElementById('main'),
       input = document.getElementById('input');
 
 function callWeatherData(zip) {
   let request = new XMLHttpRequest(),
       url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=55adfc59dc70cc96fa95a28ad5ef0526`;
+
   request.open('GET', url, true);
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
       console.log(data)
-      console.log(data.name);
-      console.log(data.weather[0]);
+      writeCityName(data.name)
+      writeWeatherIcon(data.weather[0].icon)
+      writeWeatherMain(data.weather[0].main)
       console.log(data.wind);
     } else {
       console.log("Error")
@@ -27,3 +32,14 @@ button.addEventListener('click', function() {
   callWeatherData(zip);
 })
 
+function writeCityName(cityName) {
+   city.innerHTML = `Today's weather in ${cityName}`; 
+}
+
+function writeWeatherIcon(id) {
+  icon.setAttribute("src", `http://openweathermap.org/img/w/${id}.png` );
+}
+
+function writeWeatherMain(condition) {
+  main.innerHTML = condition;
+}
