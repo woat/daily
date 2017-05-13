d1a2d12371b90f0dbdeab614cb72e9a358beb395
woat <ahmad@navlys.com>
2017-05-13 03:02:51 -0400

    fulltilt

---
+++ fulltiltcap/js/gauge.js
@@ -0,0 +1,30 @@
+var config1 = liquidFillGaugeDefaultSettings();
+config1.circleColor = "F60A20";
+config1.textColor = "#F60A20";
+config1.waveTextColor = "#F2F2F2";
+config1.waveColor = "#F60A20";
+config1.circleThickness = 0.1;
+config1.textVertPosition = 0.4;
+config1.waveAnimateTime = 1000;
+var gauge1= loadLiquidFillGauge("fillgauge1", 25.6, config1);
+
+var config2 = liquidFillGaugeDefaultSettings();
+config2.circleColor = "F60A20";
+config2.textColor = "#F60A20";
+config2.waveTextColor = "#F2F2F2";
+config2.waveColor = "#F60A20";
+config2.circleThickness = 0.1;
+config2.textVertPosition = 0.4;
+config2.waveAnimateTime = 1000;
+var gauge2= loadLiquidFillGauge("fillgauge2", 25.6, config2);
+
+var config3 = liquidFillGaugeDefaultSettings();
+config3.circleColor = "F60A20";
+config3.textColor = "#F60A20";
+config3.waveTextColor = "#F2F2F2";
+config3.waveColor = "#F60A20";
+config3.circleThickness = 0.1;
+config3.textVertPosition = 0.4;
+config3.waveAnimateTime = 1000;
+var gauge3= loadLiquidFillGauge("fillgauge3", 25.6, config3);
+
