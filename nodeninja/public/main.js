8cec0cee156efccf3e75224f5ef08383e3ceaeae
woat <ahmad@navlys.com>
2017-06-07 21:17:12 -0400

    nodeninja

--- nodeninja/public/main.js
+++ nodeninja/public/main.js
@@ -3,10 +3,21 @@ const ninjas = new Vue({
   data: {
     ninjasArray: [],
     lng: '',
-    lat: ''
+    lat: '',
+    newNinja: {
+      name: '',
+      rank: '',
+      available: false,
+      geometry: {
+        type: "point",
+        lng: '',
+        lat: ''
+      }
+    }
   },
   methods: {
     getNinjas() {
+      this.ninjasArray = [],
       fetch(`/api/ninjas?lng=${this.lng}&lat=${this.lat}`)
         .then(data => data.json())
         .then(data => {
@@ -14,7 +25,24 @@ const ninjas = new Vue({
         })
     },
     postNinjas() {
+      const postHeaders = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: this.newNinja.name,
+          rank: this.newNinja.rank,
+          available: this.newNinja.available,
+          geometry: { 
+            type: this.newNinja.type,
+            coordinates: [this.newNinja.lng, this.newNinja.lat]
+          }
+        })
+      };
 
+      fetch(`/api/ninjas`, postHeaders)
+        .then( res => res.json() )
+        .then( res => console.log(res) )
+        .catch( err => console.error(err) )
     },
     putNinjas() {
 
