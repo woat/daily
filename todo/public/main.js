0374a36a2812c4ad44007fc0031504861253055e
woat <ahmad@navlys.com>
2017-06-10 20:47:40 -0400

    todo

--- todo/public/main.js
+++ todo/public/main.js
@@ -1,17 +1,44 @@
 const todoInput = Vue.component('todo-input', {
   template: `
   <div>
-    <form>
-      <input v-model="title" type="text" placeholder="title of todo" name="title">
-      <input v-model="contents" type="text" placeholder="contents of todo" name="what do you have to do?">
-    </form>
-    <button @click="postTodo()">fuck</button>
-    <div v-for="todo in todos">
-      <ul>
-        <li>{{todo.title}}</li>
-        <li>{{todo.contents}}</li>
-      </ul>
+    <div class="container has-text-centered">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <form>
+            <input v-model="title" class="input" type="text" placeholder="title of todo" name="title">
+            <textarea v-model="contents" class="textarea" placeholder="contents of todo" name="what do you have to do?"></textarea>
+          </form>
+          <button class="button" @click="postTodo()">post it</button>
+        </div>
+      </div>
     </div>
+    <br>
+      <div v-for="todo in todos">
+        <div :id="todo._id" class="modal">
+        <div class="modal-background"></div>
+          <div class="modal-content">
+            <div class="column is-8 is-offset-2">
+              <div class="box has-text-centered">
+              <input :id="todo._id+'title'" v-model="title" class="input" type="text" placeholder="title of todo">
+              <textarea :id="todo._id+'contents'" v-model="contents" class="textarea" placeholder="contents of todo"></textarea>
+              <p class="subtitle is-5">{{todo.created}}</p>
+              <button @click="closeModal(todo._id)" class="button">close</button>
+              <button @click="putTodo(todo._id)" class="button">done</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column is-8 is-offset-2">
+          <div class="box has-text-centered">
+            <button @click="deleteTodo(todo._id)" class="delete is-pulled-right"></button>
+            <br>
+            <p class="title is-1">{{todo.title}}</p>
+            <p class="subtitle is-3">{{todo.contents}}</p>
+            <p class="subtitle is-5">{{todo.created}}</p>
+            <button @click="openModal(todo._id)" class="button">edit</button>
+          </div>
+        </div>
+      </div>
   </div>
   `,
   data: function () {
@@ -38,7 +65,52 @@ const todoInput = Vue.component('todo-input', {
 
       fetch('/api/todos')
         .then(res => res.json())
-        .then(res => res.map(todo => this.todos.push(todo)))
+        .then((res) => {
+          const temp = [];
+          res.map(todo => temp.push(todo));
+          this.todos.push(temp.slice(-1)[0]);
+        });
+    },
+    putTodo(id) {
+      const putHeaders = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: this.title,
+          contents: this.contents,
+        }),
+      };
+
+      fetch(`api/todos/${id}`, putHeaders)
+        .catch(res => console.error(res));
+
+      const targetTodo = this.todos[this.todos.indexOf(this.todos.find(todo => todo._id === id))];
+      // essentially boils to this.todos[i]
+      // pass _id in .find to get target object
+      // target object is then passed to indexOf to get the index
+      // index is then used to target the proper todo
+      targetTodo.title = this.title;
+      targetTodo.contents = this.contents;
+      this.closeModal(id);
+    },
+    deleteTodo(id) {
+      this.todos.splice(this.todos.indexOf(this.todos.find(todo => todo._id === id)), 1);
+      fetch(`api/todos/${id}`, { method: 'DELETE' })
+        .then(res => console.log(res));
+    },
+    openModal(id) {
+      const modal = document.getElementById(id);
+      const title = document.getElementById(`${id}title`);
+      const contents = document.getElementById(`${id}contents`);
+      const targetTodo = this.todos[this.todos.indexOf(this.todos.find(todo => todo._id === id))];
+
+      modal.classList.add('is-active');
+      title.value = targetTodo.title;
+      contents.value = targetTodo.contents;
+    },
+    closeModal(id) {
+      const modal = document.getElementById(id);
+      modal.classList.remove('is-active');
     },
   },
   created() {
@@ -46,14 +118,8 @@ const todoInput = Vue.component('todo-input', {
       .then(res => res.json())
       .then(res => res.map(todo => this.todos.push(todo)));
   },
-  beforeUpdate() {
-    console.log(this.todos);
-  },
 });
 
 new Vue({
   el: '#app',
-  data: {
-    message: 'Hello Vue',
-  },
 });
--- todo/routes/api.js
+++ todo/routes/api.js
@@ -15,4 +15,22 @@ router.post('/todos', (req, res) => {
     .then(todos => res.send(todos));
 });
 
+router.put('/todos/:id', (req, res) => {
+  Todo.findOne({ _id: req.params.id }, (err, doc) => {
+    if (err) res.send(err);
+    doc.title = req.body.title;
+    doc.contents = req.body.contents;
+    doc.save();
+    res.send('Put success');
+  });
+});
+
+router.delete('/todos/:id', (req, res) => {
+  Todo
+    .findByIdAndRemove(req.params.id, (err) => {
+      if (err) console.log(err);
+      res.send('Deleted successfully');
+    });
+});
+
 module.exports = router;
