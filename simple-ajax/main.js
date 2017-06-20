8bcd22eb0bb7eabc16b4d125c01dfd5177f2b9dd
pepe the frog <ahmad@navlys.com>
2017-06-19 21:43:17 -0400

    Update main.js

--- simple-ajax/main.js
+++ simple-ajax/main.js
@@ -1,8 +1,3 @@
 const ourList = document.getElementById('ourList');
 const button = document.getElementById('button');
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(data => data.json())
-  .then(data => data.map(item => {
-    console.log(item.username)
-  }))
+const url = 'https://jsonplaceholder.typicode.com/users';
