1b41c0dfd9dbc91dcd14f27517d63793063d8203
woat <ahmad@navlys.com>
2017-11-10 18:57:32 -0500

    stupidtutorials

---
+++ stupidtutorials/posts/client/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import Posts from '@/components/Posts';
+import NewPost from '@/components/NewPost';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: HelloWorld,
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts,
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost,
+    },
+  ],
+});
---
+++ stupidtutorials/posts/server/src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(cb) {
+  console.log('connection success');
+})
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/posts', (req, res) => {
+  res.send(
+    [{
+      title: 'Hello World',
+      description: 'How are you'
+    }]
+  )
+})
+
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
+app.listen(process.env.PORT || 8081);
